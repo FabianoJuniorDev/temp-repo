4a90eb84dd7dd3324f91d367a72ecd377cec6758
FabianoJuniorDev <luisfabiano_junior@hotmail.com>
2024-11-15 23:43:23 -0300

    rata

--- react-vite/src/components/Posts.tsx
+++ react-vite/src/components/Posts.tsx
@@ -6,7 +6,7 @@ export const Posts: React.FC<PostsTypes> = ({ id, title, body, cover }) => {
       <div className="posts-card">
         <img src={cover} alt={`Post ${id}`} />
         <h1>Título: {title}</h1>
-        <h2>Comentários: {body}</h2>
+        <h2>Comentário: {body}</h2>
       </div>
     </>
   );
